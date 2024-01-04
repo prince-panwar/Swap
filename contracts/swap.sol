91b1ed1d27c13ac83bd120e78b8325de92009a33
Prince <princegujjar1.pp@gmail.com>
2024-01-04 13:08:06 +0530

    clean up

--- contracts/swap.sol
+++ contracts/swap.sol
@@ -10,7 +10,7 @@ contract SwapExamples {
     ISwapRouter public constant swapRouter =
         ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
